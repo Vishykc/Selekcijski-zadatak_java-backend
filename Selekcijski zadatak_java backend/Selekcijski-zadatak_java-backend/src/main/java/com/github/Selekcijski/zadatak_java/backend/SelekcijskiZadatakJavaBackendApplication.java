9a008448187da779e7240042f1ef634ebb80e5df
Viktor Vranar <viktor.vranar@gmail.com>
2023-07-28 09:31:01 +0200

    dodani inicijalni spring initializr projekt i upute za zadatak

---
+++ Selekcijski zadatak_java backend/Selekcijski-zadatak_java-backend/src/main/java/com/github/Selekcijski/zadatak_java/backend/SelekcijskiZadatakJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.Selekcijski.zadatak_java.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelekcijskiZadatakJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelekcijskiZadatakJavaBackendApplication.class, args);
+	}
+
+}
---
+++ Selekcijski zadatak_java backend/Selekcijski-zadatak_java-backend/src/test/java/com/github/Selekcijski/zadatak_java/backend/SelekcijskiZadatakJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Selekcijski.zadatak_java.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelekcijskiZadatakJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
